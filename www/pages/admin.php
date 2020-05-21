7c513f20b48632b52cfb5451cea85e383141e696
nitram1991cz <nitrum@seznam.cz>
2020-05-21 20:12:26 +0200

    Vytvořit mock data

--- www/pages/admin.php
+++ www/pages/admin.php
@@ -3,6 +3,9 @@ include "header.php";
 ?>
 <h1> Admin </h1>
 <?php
-
+$users = array ($id = array (0,1,2,3,4),
+                $username = array ("Pepa","Franta","David","Marek","Marcel"),
+                $password = array ("heslo0","heslo1","heslo2","heslo3","heslo4"),
+                $storage_limit = array (5,10,4,3,7));
 include "footer.php";
 ?>
