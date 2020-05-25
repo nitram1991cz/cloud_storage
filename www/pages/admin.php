1d800be6175ffd9baca0ea4daa033cb9a0ae6bb5
nitram1991cz <nitrum@seznam.cz>
2020-05-25 15:36:54 +0200

    Vytvořit mock data

--- www/pages/admin.php
+++ www/pages/admin.php
@@ -10,10 +10,12 @@ include "header.php";
         "user4"=>[id=>4,username=>'Ondra',password=>'heslo4',storage_limit=>2],
         "user5"=>[id=>5,username=>'Michal',password=>'heslo5',storage_limit=>3]];
 */
-$users = array('user1'=>array('id'=>1,'username'=>'Franta','password'=>'heslo1','storage_limit'=>5),
-    "user2"=>array('id'=>2,'username'=>'Pepa','password'=>'heslo2','storage_limit'=>7),
-    "user3"=>array('id'=>3,'username'=>'Pavel','password'=>'heslo3','storage_limit'=>10),
-    "user4"=>array('id'=>4,'username'=>'Ondra','password'=>'heslo4','storage_limit'=>2),
-    "user5"=>array('id'=>5,'username'=>'Michal','password'=>'heslo5','storage_limit'=>3));
+$users = array(array('id'=>1,'username'=>'Franta','password'=>'heslo1','storage_limit'=>intval(5)),
+    array('id'=>2,'username'=>'Pepa','password'=>'heslo2','storage_limit'=>intval(7)),
+    array('id'=>3,'username'=>'Pavel','password'=>'heslo3','storage_limit'=>intval(10)),
+    array('id'=>4,'username'=>'Ondra','password'=>'heslo4','storage_limit'=>intval("")),
+    array('id'=>5,'username'=>'Michal','password'=>'heslo5','storage_limit'=>intval(3)));
+
+var_dump($users);
 include "footer.php";
 ?>
