a08a0c70dfedf0c27748fccdc481aca4a536576c
nitram1991cz <nitrum@seznam.cz>
2020-05-25 19:39:48 +0200

    Vypsat mock data

--- www/pages/admin.php
+++ www/pages/admin.php
@@ -3,28 +3,27 @@ include "header.php";
 ?>
 <h1> Admin </h1>
 <?php
-/*
- * $users = ["user1"=>[id=>1,username=>'Franta',password=>'heslo1',storage_limit=>5],
-        "user2"=>[id=>2,username=>'Pepa',password=>'heslo2',storage_limit=>7],
-        "user3"=>[id=>3,username=>'Pavel',password=>'heslo3',storage_limit=>10],
-        "user4"=>[id=>4,username=>'Ondra',password=>'heslo4',storage_limit=>2],
-        "user5"=>[id=>5,username=>'Michal',password=>'heslo5',storage_limit=>3]];
-*/
-$users = array(array('id'=>1,'username'=>'Franta','password'=>'heslo1','storage_limit'=>intval(5)),
-    array('id'=>2,'username'=>'Pepa','password'=>'heslo2','storage_limit'=>intval(7)),
-    array('id'=>3,'username'=>'Pavel','password'=>'heslo3','storage_limit'=>intval(10)),
-    array('id'=>4,'username'=>'Ondra','password'=>'heslo4','storage_limit'=>intval("")),
-    array('id'=>5,'username'=>'Michal','password'=>'heslo5','storage_limit'=>intval(3)));
 
-/*var_dump($users);*/
+$users = array( array( 'id'=>1,'username'=>'Franta','password'=>'heslo1','storage_limit'=>5 ),
+    array( 'id'=>2,'username'=>'Pepa','password'=>'heslo2','storage_limit'=>7 ),
+    array( 'id'=>3,'username'=>'Pavel','password'=>'heslo3','storage_limit'=>10 ),
+    array( 'id'=>4,'username'=>'Ondra','password'=>'heslo4','storage_limit'=>NULL ),
+    array( 'id'=>5,'username'=>'Michal','password'=>'heslo5','storage_limit'=>3 ) );
 
-echo('<table border="1">');
-foreach ($users as $index) {
-    echo('<tr><td>' . htmlspecialchars($index['username']) . '</td>');
-    echo('<td>'. htmlspecialchars($index['storage_limit']) . '</td></tr>');
+/* var_dump($users); */
+
+?>
+<table border="1">
+    <?php
+foreach ( $users as $index ) {
+?>
+    <tr><td><?php echo( htmlspecialchars( $index[ 'username' ] ) ); ?></td>
+    <td><?php echo( htmlspecialchars( $index[ 'storage_limit' ] ) ); ?></td></tr>
+    <?php
 }
-echo('</table>');
-
-
+?>
+</table>
+<?php
 include "footer.php";
 ?>
+
