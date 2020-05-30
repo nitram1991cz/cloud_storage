d4d6ff3c7daf597d6833ff7d58a0f2709e74b553
nitram1991cz <nitrum@seznam.cz>
2020-05-30 21:03:33 +0200

    Upravit tabulku na editovatelne formulare

--- www/pages/admin.php
+++ www/pages/admin.php
@@ -14,29 +14,53 @@ $users = array(array('id' => 1, 'username' => 'Franta', 'password' => 'heslo1', 
 
 ?>
 <table border="1">
-
+<span style="color: red;">
     <?php
-    foreach ($users as $index) {
-        ?>
-        <form action="admin.php" method="post">
-            <tr>
-                <td><input type="text" size="10" name="jmeno"
-                           value="<?php echo(htmlspecialchars($index['username'])); ?>"</td>
-                <td><input type="text" size="10" name="limit"
-                           value="<?php echo(htmlspecialchars($index['storage_limit'])); ?>"</td>
-                <input type=submit value=save>
-            </tr>
-            <input type="hidden" name="cislo" value="<?php echo(htmlspecialchars($index['id'])); ?>">
-        </form>
-        <?php
+    if (isset($_POST["password"])) {
+        if (strlen($_POST["password"]) < 5) {
+            echo("Heslo musi mit minimalne 5 znaku");
+        }
+        if (strlen($_POST["password"]) > 20) {
+            echo("Heslo musi mit maximalne 20 znaku");
+        }
+        if (!is_numeric($_POST["storage_limit"]) and $_POST["storage_limit"] != null) {
+            echo("Limit muze byt pouze cislo nebo prazdny retezec");
+        }
+    }
+    foreach ($users
+
+    as $index) {
+
+    ?>
+</span>
+    <form action="admin.php" method="post">
+        <tr>
+            <td><input type="text" size="10" name="username"
+                       value="<?php echo(htmlspecialchars($index['username'])); ?>"</td>
+            <td><input type="text" size="10" name="storage_limit"
+                       value="<?php echo(htmlspecialchars($index['storage_limit'])); ?>"</td>
+
+
+            <td><input type="text" size="10" name="password"
+                       value="<?php echo(htmlspecialchars($index['password'])); ?>"</td>
+
+
+            <input type=submit value=save>
+        </tr>
+        <input type="hidden" name="id" value="<?php echo(htmlspecialchars($index['id'])); ?>">
+    </form>
+    <?php
     }
     ?>
 
 </table>
+
 <?php
+
 /**echo($_POST["jmeno"]);
  * echo($_POST["limit"]);
  * echo($_POST["cislo"]);**/
 include "footer.php";
 ?>
+
 
